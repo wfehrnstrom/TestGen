b51c7716fd412fcbffca103bbb6d69997d9d097e
wfehrnstrom <wfehrnstrom@yahoo.com>
2016-05-10 22:44:10 -0700

    Created base class structure

--- src/Question.java
+++ src/Question.java
@@ -1,4 +1,8 @@
 
 public class Question {
-
+	String rawText;
+	public Question(String rawText){
+		this.rawText = rawText;
+		System.out.println(rawText);
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,19 @@
+import java.util.List;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Test {
+	List<Question> questions;
+	int numQuestions;
+	public Test(String filename){
+		
+	}
 	
+	public Test(List<Question> questions){
+		
+	}
+	
+	public ArrayList<Question> generateQuestions(){
+		return null;
+	}
 }
